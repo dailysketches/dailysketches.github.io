cffcbe0be13170a8880cabec1ef528cddf609f43
Andy McWilliams <andy@jahya.net>
2015-04-22 16:36:28 -0400

    Toggles code snippet on click

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
 	function init() {
 		initHighlightJs();
+		enableViewSnippet();
 	}
 
 	function initHighlightJs() {
 		/* jshint ignore:start */
 		hljs.initHighlightingOnLoad();
 		/* jshint ignore:end */
+	}
+
+	function enableViewSnippet() {
+		$('.snippet-button').each(function() {
+			var button = $(this);
+			var snippet = button.closest('.code').children('.snippet');
+
+			button.click(function(e) {
+				e.preventDefault();
+				snippet.slideToggle('slow');
+			});
+		});
 	}
 
 	init();

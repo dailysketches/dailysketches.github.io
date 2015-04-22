154b9c788b2c85f8214f315081024d137b12436e
Andy McWilliams <andy@jahya.net>
2015-04-22 15:44:58 -0400

    Moves highlight.js to dedicated method

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	function init() {
+		initHighlightJs();
+	}
+
+	function initHighlightJs() {
+		/* jshint ignore:start */
 		hljs.initHighlightingOnLoad();
+		/* jshint ignore:end */
 	}
 
 	init();

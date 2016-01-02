e442fa1b26ebaffe45d70c22adf8c9756e3b381c
Andrew McWilliams <andy@jahya.net>
2016-01-02 13:17:05 -0500

    Improves look and feel for new description types

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 	function init() {
 		initHighlightJs();
 		skipModernizr();
-		enableViewToggle('.snippet-button', '.code', 'snippet');
-		enableViewToggle('.pseudocode-button', '.code', 'pseudocode');
-		enableViewToggle('.examples-button', '.pattern', 'examples');
-		enableViewToggle('.description-button', '.media-description', 'description');
+		enableViewToggle('.snippet-button', '.code', '.snippet', 'snippet');
+		enableViewToggle('.pseudocode-button', '.code', '.pseudocode', 'pseudocode');
+		enableViewToggle('.examples-button', '.pattern', '.examples', 'examples');
+		enableViewToggle('.description-button', '.media-description', '.long', 'description');
 	}
 
 	function initHighlightJs() {
@@ -18,16 +18,16 @@ $(document).ready(function() {
 		$('html').removeClass('no-js').addClass('js');
 	}
 
-	function enableViewToggle(trigger, root, target) {
+	function enableViewToggle(trigger, root, targetSelector, targetName) {
 		$(trigger).each(function() {
 			var button = $(this);
-			var snippet = button.closest(root).children('.' + target);
+			var snippet = button.closest(root).children(targetSelector);
 
 			button.click(function(e) {
 				e.preventDefault();
 				var visible = snippet.is(':hidden');
 				snippet.slideToggle('slow');
-				button.text(visible ? 'hide ' + target : 'show ' + target);
+				button.text(visible ? 'hide ' + targetName : 'show ' + targetName);
 			});
 		});
 	}
